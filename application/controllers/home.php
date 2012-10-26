7f83fbc437afe5224bd57652006e5725d4cc5bd5
Bilal Quadri <bilalquadri92@gmail.com>
2012-10-25 22:36:29 -0400

    Sparks and foundations

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -3,6 +3,8 @@
 class Home extends CI_Controller {
 
   public function index() {
-    $this->load->view('home');
+    //$this->load->view('home');
+    $this->template->set_layout('default');
+    $this->template->build('home');
   }
 }
--- application/views/home.php
+++ application/views/home.php
@@ -1,11 +1,3 @@
-<html>
-<head>
-  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-
-  <title>Home</title>
-  
-</head>
-<body>
-  word
-</body>
-</html>
+<div class="panel">
+check it
+</div>

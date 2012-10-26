cce558eecab13447f22e84350cbec3aeecfeff16
Bilal Quadri <bilalquadri92@gmail.com>
2012-10-26 12:53:54 -0400

    Auto load date helper

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -68,7 +68,10 @@ $autoload['libraries'] = array(
 |	$autoload['helper'] = array('url', 'file');
 */
 
-$autoload['helper'] = array('gravatar');
+$autoload['helper'] = array(
+  'date',
+  'gravatar'
+);
 
 
 /*

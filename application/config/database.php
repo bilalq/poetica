cb98b706f7782b53ae3e5d4c3e0a16ba1703ddc2
Bilal Quadri <bilalquadri92@gmail.com>
2012-11-30 00:42:04 -0500

    Updated database credentials

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'csuser';
+$db['default']['password'] = 'cs02356d';
+$db['default']['database'] = 'poetica';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;

16fbab65c83d0be807f420e78dcdb55d2e207a73
Hua Yang <huacyang@gmail.com>
2012-12-10 18:59:35 -0500

    Not cool, erasing my changes like that, left me panicking

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -121,4 +121,35 @@ class User_model extends CI_Model {
     $result = $query->result();
     return $result;
   }
+  
+
+  public function get_people($pname, $edu, $work, $age, $gender, $country, $popular, $fans, $writing) {
+	  
+	  $pname	= empty($pname)		? "'%'" : $pname;
+	  $edu		= empty($edu)		? "'%'" : $edu;
+	  $work		= empty($work)		? "'%'" : $work;
+	  $age		= empty($age)		? "'%'" : $age;
+	  $gender	= empty($gender)	? "'%'" : $gender;
+	  $country	= empty($country)	? "'%'" : $country;
+	  $popular	= empty($popular)	? "'%'" : $popular;
+	  $fans		= empty($fans)		? "'%'" : $fans;
+	  $writing	= empty($writing)	? "'%'" : $writing;
+	  
+	$query = $this->db->query(
+	   "SELECT DISTINCT u.user_id, u.first_name, u.last_name, u.gender, u.birth_date, p.poems, p.votes, f.followers, c.comments
+		FROM Users u
+			LEFT JOIN 
+			(SELECT p.user_id, COUNT(p.user_id) AS poems, MAX(p.votes) AS votes FROM Poems p GROUP BY p.user_id) AS p ON u.user_id=p.user_id
+			LEFT JOIN
+			(SELECT f.followee, COUNT(f.follower) AS followers FROM Followings f GROUP BY f.followee) AS f ON u.user_id=f.followee
+			LEFT JOIN
+			(SELECT c.user_id, COUNT(c.user_id) AS comments FROM Comments c GROUP BY c.user_id) AS c ON u.user_id=c.user_id,
+			Employment w, Education e
+		WHERE u.user_id=w.user_id AND u.user_id=e.user_id
+		AND u.first_name like ? AND e.school like ? AND w.employer like ? AND u.birth_date > ? AND u.gender like ? AND u.country_address like ?
+		ORDER BY ?, ?;",
+		array($pname, $edu, $work, $age, $gender, $country, $popular, $writing)
+	);
+  }
+
 }

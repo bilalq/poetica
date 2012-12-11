51a8d923197ceb6ca175135aca68b590fbe78296
Alexio <alexioamota@gmail.com>
2012-12-11 05:32:01 -0500

    Extra Queries Added

--- application/models/poem_model.php
+++ application/models/poem_model.php
@@ -188,10 +188,10 @@ class Poem_model extends CI_Model {
 
       $query = $this->db->query("
         SELECT p.title, p.content, p.post_time, p.votes, 
-        p.category, u.first_name, u.last_name
+        p.category, q.first_name, q.last_name
         FROM Poems p 
         INNER JOIN 
-        (SELECT u.* FROM User u, Followings f
+        (SELECT u.* FROM Users u, Followings f
         WHERE f.followee = ? AND f.follower = u.user_id) as q
         ON p.user_id = q.user_id
         Order By p.post_time LIMIT 10;",
@@ -202,4 +202,116 @@ class Poem_model extends CI_Model {
     }
     return null;
   }
+
+  /*Finds poems with more than 5 votes*/
+  public function five_votes(){
+
+    $query = $this->db->query("
+      SELECT distinct p.*
+      FROM Poems p
+      Having Count(p.votes) > 5;
+      ");
+
+    return $query->result();
+  }
+
+  /*Average number of votes for each Poem Category*/
+  public function avg_votes(){
+
+    $query = $this->db->query("
+      SELECT p.category, AVG(p.votes)
+      From Poems p
+      GROUP BY p.category;
+      ");
+
+    return $query->result();
+  }
+
+  /* Get Writes who've written in the given poem categories*/
+  public function free_haiku($category_1, $category_2){
+
+    if(!empty($category_1) && !empty($category_2)){
+
+      $query = $this->db->query("
+        SELECT distinct u.*
+        FROM Users u, Poems p
+        WHERE u.user_id = p.user_id AND p.category = ?
+        AND u.user_id IN (SELECT distinct u.user_id 
+                          FROM Users u, Poems p
+                          WHERE u.user_id = p.user_id 
+                          AND p.category = ?); "
+        array($category_1, $category_2)
+        );
+
+      return $query->result();
+    }
+
+    return null;
+  }
+
+  /*Get all users who've written one of each category*/
+  public function all_categories(){
+
+    $query = $this->db->query("
+      SELECT u.*
+      FROM Users u
+      INNER JOIN
+      (SELECT count(*) as poem_count, u.user_id
+      FROM Users u, Poems p
+      WHERE u.user_id = p.user_id
+      GROUP BY u.user_id) as b
+      ON poem_count = 5 AND b.user_id = u.user_id;
+      ");
+
+    return $query->result();
+  }
+
+  /*Writer's whose number of written poems of a 
+  category is 5% higher than the average number 
+  of poems for that category and writer's average number of votes is 5% higher
+  then the average number of votes in said category*/
+
+  public function complex(){
+
+    $query = $this->db->query("
+
+    SELECT u.*
+    FROM Users u,
+    (SELECT b.*
+    FROM (SELECT AVG(p.votes) as avgCat, p.category
+      FROM Poems p
+      GROUP BY p.category
+      ) as a,
+      (SELECT u.*, AVG(p.votes) as avgVotes , p.category
+      FROM Users u, Poems p
+      WHERE u.user_id = p.user_id
+      GROUP BY u.user_id AND p.category) as b
+      WHERE b.avgVotes >= ((a.avgCat*.03)+a.avgCat) 
+            AND b.category = a.category) as con2,
+    (SELECT u.*, COUNT(*) as pcount , p.category
+    FROm Users u, Poems p
+    WHERE u.user_id = p.user_id
+    GROUP BY u.user_id AND p.category) as a,
+    (SELECT AVG(b.pcount) as avgCount, b.category
+    FROM (SELECT u.*, COUNT(*) as pcount, p.category
+    FROm Users u, Poems p
+    WHERE u.user_id = p.user_id
+    GROUP BY u.user_id AND p.category) as b
+    GROUP BY b.category) as b2
+    WHERE a.pcount >= ((b2.avgCount*.05)+b2.avgCount)
+    AND a.category = b2.category) as con1
+    WHERE u.user_id=con1.user_id AND u.user_id=con2.user_id
+    AND con1.user_id=con2.user_id
+
+
+
+
+
+      ")
+
+    return $query->result();
+  }
+
+
+
 }
